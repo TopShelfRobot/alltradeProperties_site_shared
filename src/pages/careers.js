64387a4eb5680e81f6226085a055c66e2a5eb0c9
Aaron Johnson <aarontropy@gmail.com>
2019-01-03 10:45:26 -0500

    wrap careers page in container

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import Container from '../components/container'
 
 export default class CareersPage extends React.Component {
   componentDidMount() {
@@ -16,7 +17,7 @@ export default class CareersPage extends React.Component {
   render() {
     return (
       <Layout pageTitle="Careers">
-        <div id="newton-container" />
+        <Container id="newton-container" />
       </Layout>
     )
   }
