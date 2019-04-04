78b222ee4664a27007f1d581c6b2ec09f15c7e30
Aaron Johnson <aarontropy@gmail.com>
2019-04-04 09:06:25 -0400

    remove twitter and facebook feeds

--- src/components/footer/footer-top.js
+++ src/components/footer/footer-top.js
@@ -34,10 +34,12 @@ const FooterTop = props => (
       return (
         <div className="footer-top">
           <Container>
-            <Row>
-              <div className="col-sm-4 logo-box">
+            <Row style={{ justifyContent: 'center', alignItems: 'center' }}>
+              <div className="col-sm-4 logo-box align-self-center">
                 <Img className="logo" fluid={logoImageData} />
+              </div>
 
+              <div className="col-sm-4">
                 <Row>
                   <div className="col-sm-4">
                     <img className="eho" src={ehoLogo} alt="Equal Housing Opportunity Logo" />
@@ -47,76 +49,6 @@ const FooterTop = props => (
                   </div>
                 </Row>
               </div>
-
-              <div className="col-sm-4 twitter-wrapper">
-                <h5>Follow Alltrade on Twitter</h5>
-
-                <div className="twitter-container">
-                  <a className="twitter-timeline" href="https://twitter.com/alltradeky">
-                    Tweets by @alltradeky
-                  </a>
-                  <script async src="//platform.twitter.com/widgets.js" />
-                </div>
-              </div>
-
-              <div className="col-sm-4 facebook-wrapper">
-                <h5>Follow Alltrade on Facebook</h5>
-                {process.env.NODE_ENV !== 'development' ? (
-                  <iframe
-                    title="Alltrade Facebook"
-                    src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Falltradeproperties&amp;tabs=timeline&amp;height=250&amp;small_header=true&amp;adapt_container_width=true&amp;hide_cover=true&amp;show_facepile=false&amp;appId"
-                    width="100%"
-                    height="250"
-                    style={{ border: 'none', overflow: 'hidden' }}
-                    scrolling="no"
-                    frameBorder="0"
-                    allowtransparency="true"
-                  />
-                ) : (
-                  <span>FB Disabled in Development mode</span>
-                )}
-              </div>
-              {/* <div className="text-widget col-sm-4 logo-box">
-              <div className="logos">
-                <div>
-                  <img src="/images/logo-header.png" alt="" />
-                </div>
-    
-                <div className="row">
-                  <div className="col-sm-4">
-                    <img className="eho-logo" src="/images/eho_logo.png" alt="" />
-                  </div>
-    
-                  <div className="col-sm-8">
-                    
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-4 twitter-wrapper">
-              <h5>Follow Alltrade on Twitter</h5>
-    
-              <div className="twitter-container">
-                <a className="twitter-timeline" href="https://twitter.com/alltradeky">
-                  Tweets by @alltradeky
-                </a>
-                <script async src="//platform.twitter.com/widgets.js" />
-              </div>
-            </div>
-    
-            <div className="col-sm-4 small-form facebook-wrapper">
-              <h5>Follow Alltrade on Facebook</h5>
-              <iframe
-                title="Alltrade Facebook Feed"
-                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Falltradeproperties&tabs=timeline&height=250&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false&appId"
-                width="100%"
-                height="250"
-                style={{ border: 'none', overflow: 'hidden' }}
-                scrolling="no"
-                frameBorder="0"
-                allowtransparency="true"
-              />
-            </div> */}
             </Row>
           </Container>
         </div>
